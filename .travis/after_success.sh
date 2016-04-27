9d469714de114d862c2d372088535a85c0f1387f
madhead <siarhei.krukau@gmail.com>
2016-04-27 20:10:33 +0300

    Upload only releases

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,5 +2,9 @@
 
 # Only upload artifacts from upstream's master build on Oracle JDK 7
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${JAVA_HOME} == "/usr/lib/jvm/java-8-oracle" ]]; then
-	./gradlew bintrayUpload
+	if grep -q "SNAPSHOT" gradle.properties; then
+	else
+		echo "Uploading RELEASE to https://bintray.com/"
+		./gradlew bintrayUpload
+	fi
 fi
