78ddc6c4788f8a561117f096d7125976f69b7fa1
madhead <siarhei.krukau@gmail.com>
2016-04-27 20:26:08 +0300

    Releases, tags...

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# Only upload artifacts from upstream's master build on Oracle JDK 7
-if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${JAVA_HOME} == "/usr/lib/jvm/java-8-oracle" ]]; then
-	if ! grep -q "SNAPSHOT" gradle.properties; then
-		echo "Uploading RELEASE to https://bintray.com/"
-		./gradlew bintrayUpload
-	fi
+# Only upload tagged artifacts build on Oracle JDK 7
+if [[ ! -z ${TRAVIS_TAG} ]] && [[ ${JAVA_HOME} == "/usr/lib/jvm/java-8-oracle" ]]; then
+	echo "Uploading RELEASE to https://bintray.com/"
+	./gradlew bintrayUpload
 fi
