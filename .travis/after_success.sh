9164e88b484b979bfa52ff1f61c353f6ee771632
madhead <siarhei.krukau@gmail.com>
2016-04-27 19:37:39 +0300

    Fix shebangs

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Only upload artifacts from upstream's master build on Oracle JDK 7
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${JAVA_HOME} == "/usr/lib/jvm/java-8-oracle" ]]; then
