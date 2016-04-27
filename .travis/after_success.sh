f949970ee5c583126c3829e7ba1232d134ecccbf
madhead <siarhei.krukau@gmail.com>
2016-04-27 20:14:02 +0300

    Fix syntax

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,8 +2,7 @@
 
 # Only upload artifacts from upstream's master build on Oracle JDK 7
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${JAVA_HOME} == "/usr/lib/jvm/java-8-oracle" ]]; then
-	if grep -q "SNAPSHOT" gradle.properties; then
-	else
+	if ! grep -q "SNAPSHOT" gradle.properties; then
 		echo "Uploading RELEASE to https://bintray.com/"
 		./gradlew bintrayUpload
 	fi
