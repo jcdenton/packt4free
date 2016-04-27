89b391cce05af6e451c7a0f424394ad78455d59e
madhead <siarhei.krukau@gmail.com>
2016-04-27 19:52:41 +0300

    What's the problem with it?

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,5 +2,5 @@
 
 # Only upload artifacts from upstream's master build on Oracle JDK 7
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${JAVA_HOME} == "/usr/lib/jvm/java-8-oracle" ]]; then
-	./gradlew bintrayUpload
+	./gradlew assembleDist bintrayUpload
 fi
